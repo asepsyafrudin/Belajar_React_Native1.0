96066b83b5b547e81dc4c9722257471a0431cad0
DNIA-DX <DNIA-DX@P550123001.ap.denso.com>
2024-06-11 10:05:23 +0700

    add card and scroll view

--- app/Home/details.js
+++ app/Home/details.js
@@ -1,43 +1,55 @@
 import axios from "axios";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import { useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Card from "../../components/Card/card";
 
 export default function Details() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios.get("https://dummyjson.com/products").then((res) => {
-      setData(res.data);
-      console.log(res.data);
+      setData(res.data.products);
+      // console.log(res.data);
     });
   }, []);
 
   const router = useRouter();
   const params = useLocalSearchParams();
+  const [order, setOrder] = useState([]);
+
+  const handleDecrement = (id) => {
+    console.log(id);
+  };
+  const handleIncrement = (id) => {
+    console.log(id);
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView>
       <Stack.Screen
         options={{
           title: params.name,
         }}
       />
-      <Text
-        onPress={() => {
-          router.setParams({ name: "Updated" });
-        }}
-      >
-        Update the title
-      </Text>
-    </View>
+      {console.log(data)}
+      {data.length > 0 &&
+        data.map((item) => (
+          <Card
+            item={item}
+            onIncrement={handleIncrement}
+            onDecrement={handleDecrement}
+            key={item.id}
+          />
+        ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
 });
---
+++ components/Card/card.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+const Card = ({ item, onIncrement, onDecrement }) => {
+  return (
+    <View style={styles.productCard}>
+      <Image source={{ uri: item.images[0] }} style={styles.productImage} />
+      <View style={styles.productInfo}>
+        <Text style={styles.productName}>{item.title}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
+        <Text style={styles.productPrice}>
+          ${item.price.toFixed(2)}{" "}
+          <Text style={styles.productPriceText}>per item</Text>
+        </Text>
+      </View>
+      <View style={styles.productAmount}>
+        <TouchableOpacity style={styles.amountButton} onPress={onDecrement}>
+          <Text style={styles.amountButtonText}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.amountText}>{item.amount}</Text>
+        <TouchableOpacity style={styles.amountButton} onPress={onIncrement}>
+          <Text style={styles.amountButtonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f7f7f7",
+    paddingTop: 40,
+  },
+  productList: {
+    flex: 1,
+    paddingTop: 16,
+  },
+  productCard: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    padding: 16,
+    marginBottom: 16,
+  },
+  productImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 16,
+  },
+  productInfo: {
+    flex: 1,
+    marginRight: 16,
+  },
+  productName: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 4,
+  },
+  productDescription: {
+    fontSize: 14,
+    color: "#666",
+    marginBottom: 4,
+  },
+  productPrice: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#4caf50",
+  },
+  productPriceText: {
+    fontSize: 14,
+    fontWeight: "normal",
+    color: "#666",
+  },
+  productAmount: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  amountButton: {
+    width: 30,
+    height: 30,
+    backgroundColor: "#ffa726",
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  amountButtonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  amountText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginHorizontal: 16,
+  },
+  continueButton: {
+    position: "absolute",
+    bottom: 16,
+    left: 16,
+    right: 16,
+    backgroundColor: "#4caf50",
+    borderRadius: 8,
+    padding: 16,
+    alignItems: "center",
+  },
+  continueButtonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default Card;
